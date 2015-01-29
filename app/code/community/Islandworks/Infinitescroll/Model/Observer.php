1d8b7ad240349a3762919d41662bf21a915b5396
Dzung Nova <dzungnovaz@gmail.com>
2015-01-30 00:18:54 +0700

    add infinite scroll

---
+++ app/code/community/Islandworks/Infinitescroll/Model/Observer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * InfiniteScroll - Magento Integration
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0),
+ * available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * @category   Islandworks
+ * @package    Islandworks_Infinitescroll
+ * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @copyright  Copyright (c) 2014 Islandworks Inc. (http://useislandworks.com)
+ *
+ * @author     Gabriel Somoza (me@gabrielsomoza.com)
+ * @link       http://gabrielsomoza.com/
+ *
+ * Update 2.0.0
+ * @author     Enrique Piatti (contacto@enriquepiatti.com)
+ * @link       http://www.dwdesigner.com/
+ */
+class Islandworks_Infinitescroll_Model_Observer {
+
+}
